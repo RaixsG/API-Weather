ee6bd4484098cc468c53dd99fb2ef1a9560e140d
RaixsG <jhardelloficial@gmail.com>
2023-08-17 00:21:49 -0500

    feat: depent normalize and CSS global

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
-    import { getWeatherFrom } from '../service/weather'
+    import { getWeatherFrom } from "../service/weather";
     const weatherPromise = getWeatherFrom();
-
 </script>
 
 {#await weatherPromise then weather}
@@ -9,6 +8,22 @@
 {/await}
 
 <style>
+    /* apply a natural box layout model to all elements, but allowing components to change */
+    :global(html) {
+        box-sizing: border-box;
+    }
+
+    *,
+    *:before,
+    *:after {
+        box-sizing: inherit;
+    }
+
+    :global(body) {
+        background-color: #222;
+        color: #f5f5f5;
+    }
+
     h1 {
         font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
             Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
